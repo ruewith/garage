a6a3aa439fad1d81480d3824c35d7cbf1b5498fb
ruewith <ruewith@gmail.com>
2020-03-16 15:55:12 +0300

    commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,34 @@
 $(document).ready(function() {
     new WOW().init();
+
+    let firstname = document.getElementById("client_firstname");
+    let lastname = document.getElementById("client_lastname");
+    let phone = document.getElementById("client_phone");
+    let mileage = document.getElementById("mileage");
+
+    if (firstname) {
+        let firstnameMask = new IMask(firstname, {
+            mask: /^([а-яё]+|[А-ЯЁ]+)$/i
+        });
+    }
+    if (lastname) {
+        let lastnameMask = new IMask(lastname, {
+            mask: /^([а-яё]+|[А-ЯЁ]+)$/i
+        });
+    }
+
+    if (phone) {
+        let phoneMask = new IMask(phone, {
+            mask: "+{7}(000)000-00-00"
+        });
+    }
+
+    if (mileage) {
+        let mileageMask = new IMask(mileage, {
+            mask: Number,
+            min: 0,
+            max: 1000000,
+            thousandsSeparator: " "
+        });
+    }
 });
